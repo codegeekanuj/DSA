9a6e89896eba4ed383eeeffbe1d11f1dfde276ac
anuj tripathi <atripathi1253@gmail.com>
2024-01-04 23:50:44 +0530

    armstrong

--- basic/armstrong.java
+++ basic/armstrong.java
@@ -1,23 +1,26 @@
-import java.util.*;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class armstrong {
-	
-	public static void main(String[] args) {
-		// Write your code here
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int t=n;
-		int s=0;
-		int digits=(int)(log10(t)+1);
-		while(t!=0){
-			int rem=t%10;
-			s=s+Math.pow(rem, digits);
-			t=t/10;
-		}
-		if(s==n)
-		return true;
-        
-		
-		return false;
-	}
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = n;
+        int s = 0;
+        int digits = (int)(Math.log10(t) + 1);
+
+        while (t != 0) {
+            int rem = t % 10;
+            s = s + (int)Math.pow(rem, digits);
+            t = t / 10;
+        }
+
+        if (s == n) {
+            System.out.println(n + " is an Armstrong number.");
+        } else {
+            System.out.println(n + " is not an Armstrong number.");
+        }
+		sc.close();
+    }
 }
